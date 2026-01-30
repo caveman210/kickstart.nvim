e20ec5fc02e774733b2a0cbced99c9b64226903c
George Sandeep <illustrio7077@gmail.com>
2026-01-31 00:25:45 +0530

    Modified kickstart to external, and added a new uv plugin

--- init.lua
+++ init.lua
@@ -1069,12 +1069,13 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'external.plugins.debug',
+  require 'external.plugins.indent_line',
+  require 'external.plugins.lint',
+  require 'external.plugins.autopairs',
+  require 'external.plugins.neo-tree',
+  require 'external.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'external.plugins.uv',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
